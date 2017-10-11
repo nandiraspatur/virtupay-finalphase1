e16be2dbf21b907403f983ae8b201ec31d79b9a3
Nandira Paturohman <nandirasp@gmail.com>
2017-10-11 17:43:25 +0700

    views 0.3

--- app.js
+++ app.js
@@ -7,13 +7,16 @@ app.use(express.static('./public'))
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
 const index = require('./routes/index');
 const user = require('./routes/user');
 const product = require('./routes/product');
+const transaction = require('./routes/transaction');
 
 app.use('/', index);
 app.use('/users', user);
 app.use('/products', product);
+app.use('/transactions', transaction);
 
 app.listen(process.env.PORT || '3000', () => {
   console.log('Listening port 3000');
