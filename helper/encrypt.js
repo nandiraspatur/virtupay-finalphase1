21e850bb0018392bb7be76f704c4f802a4c57867
Nandira Paturohman <nandirasp@gmail.com>
2017-10-13 11:21:30 +0700

    remove

--- helper/encrypt.js
+++
@@ -1,9 +0,0 @@
-const crypto = require('crypto')
-module.exports = function (password, secret) {
-    algorithm = 'aes-256-ctr';
-
-    var cipher = crypto.createCipher(algorithm, secret)
-    var crypted = cipher.update(password, 'utf8', 'hex')
-    crypted += cipher.final('hex');
-    return crypted;
-}
